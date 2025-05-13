41c20991fd885af5275e54f30c82b6fdf66f8bd0
Felipe <nobreakfast1321@gmail.com>
2025-05-13 17:53:45 -0300

    Create tests for category

---
+++ test/models/category_test.rb
@@ -0,0 +1,60 @@
+require "test_helper"
+
+class CategoryTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:one)
+    @category = Category.new(
+      visible: true,
+      name: "Test Name",
+      description: "Test Description",
+      user: @user
+    )
+  end
+
+  test "should be valid" do
+    assert @category.valid?
+  end
+
+  test "should normalize name before validation" do
+    @category.name = "  #{@category.name}  "
+    @category.valid?
+    assert_equal "#{@category.name}", @category.name
+  end
+
+  test "visible should be true or false" do
+    @category.visible = true
+    assert @category.valid?
+
+    @category.visible = false
+    assert @category.valid?
+
+    @category.visible = nil
+    assert_not @category.valid?
+  end
+
+  test "name should have a maximum length of 30 characters" do
+    @category.name = "a" * 31
+    assert_not @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = nil
+    assert_not @category.valid?
+  end
+
+  test "name should be unique per user" do
+    category_clone = @category.dup
+    @category.save
+    assert_not category_clone.valid?
+  end
+
+  test "description should have a maximum length of 150 characters" do
+    @category.description = "a" * 151
+    assert_not @category.valid?
+  end
+
+  test "description can be nil" do
+    @category.description = nil
+    assert @category.valid?
+  end
+end
