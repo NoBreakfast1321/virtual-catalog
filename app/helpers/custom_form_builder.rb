b1afb379efeeed7127ce40cb93471913f6b0e4ad
Felipe <nobreakfast1321@gmail.com>
2025-07-08 10:35:45 -0300

    Favor the usage of empty? instead of any?

--- app/helpers/custom_form_builder.rb
+++ app/helpers/custom_form_builder.rb
@@ -1,6 +1,6 @@
 class CustomFormBuilder < ActionView::Helpers::FormBuilder
   def error(field, klass = "")
-    return unless object.errors[field].any?
+    return if object.errors[field].empty?
 
     @template.content_tag(:div, object.errors.full_messages_for(field).to_sentence, class: klass)
   end
