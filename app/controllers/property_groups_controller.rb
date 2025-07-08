ecc78d41e6fe677b9bd881f80f20d383464214c7
Felipe <nobreakfast1321@gmail.com>
2025-07-08 10:35:03 -0300

    Do not rebuild variants if property group is empty when destroyed

--- app/controllers/property_groups_controller.rb
+++ app/controllers/property_groups_controller.rb
@@ -39,7 +39,7 @@ class PropertyGroupsController < ApplicationController
   def destroy
     respond_to do |format|
       if @property_group.destroy
-        Products::VariantsRebuilder.call(@product)
+        Products::VariantsRebuilder.call(@product) unless @property_group.properties.empty?
 
         format.turbo_stream { flash.now[:notice] = "Property group was successfully destroyed." }
       else
