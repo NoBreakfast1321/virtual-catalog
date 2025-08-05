75143c9eb568f01489c68de68528fb8b9b8becdc
Felipe <nobreakfast1321@gmail.com>
2025-08-05 13:08:52 -0300

    Refactor and format Products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,63 +3,50 @@ class ProductsController < ApplicationController
 
   before_action :set_business
   before_action :set_product, only: %i[show edit update destroy]
+  before_action :set_product_with_params, only: %i[create]
 
   before_action :restrict_product_creation, only: %i[new create]
 
-  # GET /businesses/:business_id/products
   def index
     @q = @business.products.ransack(params[:q])
     @pagy, @products = pagy(@q.result(distinct: true))
   end
 
-  # GET /businesses/:business_id/products/:id
   def show
   end
 
-  # GET /businesses/:business_id/products/new
   def new
     @product = @business.products.build
   end
 
-  # GET /businesses/:business_id/products/:id/edit
   def edit
   end
 
-  # POST /businesses/:business_id/products
   def create
-    @product = @business.products.build(product_params)
-
     # Attach newly uploaded images to the product.
     if params[:product][:images]
       @product.images.attach(params[:product][:images])
     end
 
+    ActiveRecord::Base.transaction do
+      @product.save!
+
+      @product.variants.create!(
+        base: true,
+        code: "#{params[:product][:code]}-BASE",
+        price: params[:product][:price],
+        visible: params[:product][:visible],
+      )
+    end
+
     respond_to do |format|
-      begin
-        ActiveRecord::Base.transaction do
-          @product.save!
-
-          @product.variants.create!(
-            base: true,
-            code: "#{params[:product][:code]}-BASE",
-            price: params[:product][:price],
-            visible: params[:product][:visible],
-          )
-        end
-
-        format.html do
-          redirect_to [ @business, @product ],
-                      notice: t_controller("create.success")
-        end
-      rescue ActiveRecord::RecordInvalid => error
-        flash.now[:alert] = error.record.errors.full_messages.to_sentence
-
-        format.html { render :new, status: :unprocessable_entity }
+      format.html do
+        redirect_to [ @business, @product ],
+                    notice: t_controller("create.success")
       end
     end
   end
 
-  # PATCH/PUT /businesses/:business_id/products/:id
   def update
     # Purge images that were marked for deletion.
     if (image_ids = params.dig(:product, :purge_image_ids).presence)
@@ -71,45 +58,32 @@ class ProductsController < ApplicationController
       @product.images.attach(params[:product][:images])
     end
 
+    ActiveRecord::Base.transaction do
+      @product.update!(product_params)
+
+      @product.base_variant.update!(
+        code: params[:product][:code],
+        price: params[:product][:price],
+        visible: params[:product][:visible],
+      )
+    end
+
     respond_to do |format|
-      begin
-        ActiveRecord::Base.transaction do
-          @product.update!(product_params)
-
-          @product.base_variant.update!(
-            code: params[:product][:code],
-            price: params[:product][:price],
-            visible: params[:product][:visible],
-          )
-        end
-
-        format.html do
-          redirect_to [ @business, @product ],
-                      notice: t_controller("update.success")
-        end
-      rescue ActiveRecord::RecordInvalid => error
-        flash.now[:alert] = error.record.errors.full_messages.to_sentence
-
-        format.html { render :edit, status: :unprocessable_entity }
+      format.html do
+        redirect_to [ @business, @product ],
+                    notice: t_controller("update.success")
       end
     end
   end
 
-  # DELETE /businesses/:business_id/products/:id
   def destroy
-    respond_to do |format|
-      if @product.destroy
-        format.html do
-          redirect_to business_products_path(@business),
-                      notice: t_controller("destroy.success"),
-                      status: :see_other
-        end
-      else
-        format.html do
-          flash.now[:alert] = @product.errors.full_messages.to_sentence
+    @product.destroy!
 
-          render :show, status: :unprocessable_entity
-        end
+    respond_to do |format|
+      format.html do
+        redirect_to business_products_path(@business),
+                    notice: t_controller("destroy.success"),
+                    status: :see_other
       end
     end
   end
@@ -123,6 +97,10 @@ class ProductsController < ApplicationController
 
   def set_product
     @product = @business.products.find(params.expect(:id))
+  end
+
+  def set_product_with_params
+    @product = @business.products.build(product_params)
   end
 
   # Only allow a list of trusted parameters through.
