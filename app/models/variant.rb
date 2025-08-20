24ccb70814a3fa71e0178916e1008ba2f522d9fa
Felipe <nobreakfast1321@gmail.com>
2025-08-20 11:18:06 -0300

    Add more validations to variant model

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -18,8 +18,8 @@
 #
 #  index_variants_on_product_id                (product_id)
 #  index_variants_on_product_id_and_base       (product_id) UNIQUE WHERE base = 1
-#  index_variants_on_product_id_and_code       (product_id,code) UNIQUE WHERE NULLIF(TRIM(code), '') IS NOT NULL
-#  index_variants_on_product_id_and_signature  (product_id,signature) UNIQUE WHERE base = 0 AND NULLIF(TRIM(signature), '') IS NOT NULL
+#  index_variants_on_product_id_and_code       (product_id,code) UNIQUE
+#  index_variants_on_product_id_and_signature  (product_id,signature) UNIQUE WHERE base = 0 AND signature IS NOT NULL
 #
 # Foreign Keys
 #
@@ -67,9 +67,10 @@ class Variant < ApplicationRecord
   validates :signature,
             presence: true,
             uniqueness: {
-              scope: :product_id
+              scope: :product_id,
+              conditions: -> { where(base: false) }
             },
-            allow_blank: true
+            unless: :base?
 
   validates :stock_quantity,
             numericality: {
@@ -81,8 +82,10 @@ class Variant < ApplicationRecord
   validates :visible, inclusion: { in: [ true, false ] }
 
   validate :ensure_one_selection_per_group
+  validate :product_must_have_property_group, on: :create, unless: :base?
+  validate :relocate_signature_error, if: -> { errors[:signature].any? }
 
-  before_validation :generate_signature, on: %i[create update]
+  before_validation :generate_signature, on: %i[create update], unless: :base?
 
   scope :base, -> { where(base: true) }
   scope :non_base, -> { where(base: false) }
@@ -96,23 +99,45 @@ class Variant < ApplicationRecord
   def ensure_one_selection_per_group
     return if product.nil?
 
-    required_group_ids = product.property_groups.ids
+    expected_group_ids = product.property_groups.ids
 
-    return if required_group_ids.empty?
+    return if expected_group_ids.empty?
 
-    selected_group_ids = properties.map(&:property_group_id).uniq.compact
+    provided_group_ids = properties.map(&:property_group_id).uniq.compact
 
-    missing_group_ids = required_group_ids - selected_group_ids
+    excluded_group_ids = expected_group_ids - provided_group_ids
 
-    return if missing_group_ids.empty?
+    return if excluded_group_ids.empty?
 
     errors.add(:property_ids, :must_select_property)
   end
 
   def generate_signature
-    return if base?
-
-    self.signature = properties.by_group_earliest_created.map(&:id).join("-")
+    self.signature = ordered_properties.map(&:id).join("-")
   end
+
+  def ordered_properties
+    properties.to_a.sort_by do |property|
+      property_group = property.property_group
+
+      [
+        property_group&.created_at || Time.at(0),
+        property_group&.id || 0,
+        property.id || 0
+      ]
+    end
+  end
+
+  def product_must_have_property_group
+    return if product&.property_groups&.exists?
+
+    # TODO: Find a way to better display this error in the UI.
+    errors.add(:base, :product_without_property_group)
+  end
+
+  # TODO: Remove once UI properly displays error messages (condensed form has limited space).
+  def relocate_signature_error
+    errors.delete(:signature)
+    errors.add(:property_ids, :property_combination_already_used)
   end
 end
