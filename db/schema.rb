2410eb5eff67bb8b791be5fe2473e730b604e1f4
Felipe <nobreakfast1321@gmail.com>
2025-08-21 14:55:10 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -62,58 +62,58 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_21_123834) do
   end
 
   create_table "businesses", force: :cascade do |t|
-    t.text "description", limit: 150
+    t.integer "user_id", null: false
     t.string "name", limit: 30, null: false
     t.string "slug", limit: 30, null: false
+    t.text "description", limit: 150
     t.boolean "visible", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
     t.index ["slug"], name: "index_businesses_on_slug", unique: true
     t.index ["user_id", "name"], name: "index_businesses_on_user_id_and_name", unique: true
     t.index ["user_id"], name: "index_businesses_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.text "description", limit: 150
+    t.integer "business_id", null: false
     t.string "name", limit: 30, null: false
+    t.text "description", limit: 150
     t.boolean "visible", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "business_id", null: false
     t.index ["business_id", "name"], name: "index_categories_on_business_id_and_name", unique: true
     t.index ["business_id"], name: "index_categories_on_business_id"
   end
 
   create_table "option_groups", force: :cascade do |t|
-    t.integer "maximum_selections"
-    t.integer "minimum_selections", default: 1, null: false
+    t.integer "business_id", null: false
     t.string "name", limit: 30, null: false
+    t.integer "minimum_selections", default: 1, null: false
+    t.integer "maximum_selections"
     t.boolean "visible", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "business_id", null: false
     t.index ["business_id", "name"], name: "index_option_groups_on_business_id_and_name", unique: true
     t.index ["business_id"], name: "index_option_groups_on_business_id"
   end
 
   create_table "options", force: :cascade do |t|
+    t.integer "option_group_id", null: false
     t.string "name", limit: 50, null: false
     t.integer "price_cents", default: 0, null: false
     t.string "price_currency", default: "USD", null: false
     t.boolean "visible", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "option_group_id", null: false
     t.index ["option_group_id", "name"], name: "index_options_on_option_group_id_and_name", unique: true
     t.index ["option_group_id"], name: "index_options_on_option_group_id"
   end
 
   create_table "product_categories", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "product_id", null: false
     t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["category_id", "product_id"], name: "index_product_categories_on_category_id_and_product_id"
     t.index ["category_id"], name: "index_product_categories_on_category_id"
     t.index ["product_id", "category_id"], name: "index_product_categories_on_product_id_and_category_id", unique: true
@@ -121,10 +121,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_21_123834) do
   end
 
   create_table "product_option_groups", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "product_id", null: false
     t.integer "option_group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["option_group_id", "product_id"], name: "index_product_option_groups_on_option_group_id_and_product_id"
     t.index ["option_group_id"], name: "index_product_option_groups_on_option_group_id"
     t.index ["product_id", "option_group_id"], name: "index_product_option_groups_on_product_id_and_option_group_id", unique: true
@@ -132,10 +132,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_21_123834) do
   end
 
   create_table "product_property_groups", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "product_id", null: false
     t.integer "property_group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["product_id", "property_group_id"], name: "idx_on_product_id_property_group_id_a2903aef74", unique: true
     t.index ["product_id"], name: "index_product_property_groups_on_product_id"
     t.index ["property_group_id", "product_id"], name: "idx_on_property_group_id_product_id_431a475453"
@@ -143,18 +143,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_21_123834) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.boolean "adult_only", default: false, null: false
-    t.datetime "available_from"
-    t.datetime "available_until"
-    t.string "code", limit: 50
-    t.text "description", limit: 5000
-    t.boolean "featured", default: false, null: false
+    t.integer "business_id", null: false
     t.string "name", limit: 150, null: false
     t.string "slug", limit: 150, null: false
+    t.string "code", limit: 50
+    t.text "description", limit: 5000
+    t.boolean "adult_only", default: false, null: false
+    t.boolean "featured", default: false, null: false
     t.boolean "visible", default: true, null: false
+    t.datetime "available_from"
+    t.datetime "available_until"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "business_id", null: false
     t.index ["business_id", "code"], name: "index_products_on_business_id_and_code", unique: true
     t.index ["business_id", "name"], name: "index_products_on_business_id_and_name", unique: true
     t.index ["business_id", "slug"], name: "index_products_on_business_id_and_slug", unique: true
@@ -162,19 +162,19 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_21_123834) do
   end
 
   create_table "properties", force: :cascade do |t|
+    t.integer "property_group_id", null: false
     t.string "name", limit: 50, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "property_group_id", null: false
     t.index ["property_group_id", "name"], name: "index_properties_on_property_group_id_and_name", unique: true
     t.index ["property_group_id"], name: "index_properties_on_property_group_id"
   end
 
   create_table "property_groups", force: :cascade do |t|
+    t.integer "business_id", null: false
     t.string "name", limit: 30, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "business_id", null: false
     t.index ["business_id", "name"], name: "index_property_groups_on_business_id_and_name", unique: true
     t.index ["business_id"], name: "index_property_groups_on_business_id"
   end
@@ -192,10 +192,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_21_123834) do
   end
 
   create_table "variant_properties", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "variant_id", null: false
     t.integer "property_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["property_id", "variant_id"], name: "index_variant_properties_on_property_id_and_variant_id"
     t.index ["property_id"], name: "index_variant_properties_on_property_id"
     t.index ["variant_id", "property_id"], name: "index_variant_properties_on_variant_id_and_property_id", unique: true
@@ -203,16 +203,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_21_123834) do
   end
 
   create_table "variants", force: :cascade do |t|
-    t.boolean "base", default: false, null: false
+    t.integer "product_id", null: false
     t.string "code", limit: 50
+    t.string "signature"
     t.integer "price_cents", default: 0, null: false
     t.string "price_currency", default: "USD", null: false
-    t.string "signature"
     t.integer "stock_quantity"
+    t.boolean "base", default: false, null: false
     t.boolean "visible", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "product_id", null: false
     t.index ["product_id", "code"], name: "index_variants_on_product_id_and_code", unique: true
     t.index ["product_id", "signature"], name: "index_variants_on_product_id_and_signature", unique: true, where: "base = 0 AND signature IS NOT NULL"
     t.index ["product_id"], name: "index_variants_on_product_id"
