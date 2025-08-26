4674ced7c57a6828f3929c1c6602f55c33317f80
Felipe <nobreakfast1321@gmail.com>
2025-08-26 11:59:17 -0300

    Create cart controller

---
+++ app/javascript/controllers/cart_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["select"];
+
+  cart = [];
+
+  add() {
+    const variant = JSON.parse(this.selectTarget.value || null);
+
+    if (!variant) {
+      return;
+    }
+
+    const cartItem = this.cart.find((item) => {
+      return item.id === variant.id;
+    });
+
+    if (cartItem) {
+      cartItem.quantity += 1;
+    } else {
+      this.cart.push({ ...variant, quantity: 1 });
+    }
+
+    console.log(this.cart);
+  }
+}
