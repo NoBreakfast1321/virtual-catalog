e8dd72e8732523d69350a22f43d717cf95c9c7ca
Felipe <nobreakfast1321@gmail.com>
2025-08-28 13:44:56 -0300

    Format routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -129,7 +129,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", :as => :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
@@ -151,7 +151,7 @@ Rails.application.routes.draw do
     end
 
     resources :products do
-      resource :product_images, only: [ :update ]
+      resource :product_images, only: %i[update]
 
       resources :product_option_groups, only: %i[new create destroy]
       resources :product_property_groups, only: %i[new create destroy]
